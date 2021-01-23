2ea714d5a68026d122df2c5d0d288b40efb08642
cactusHats <mickeymasashi@gmail.com>
2021-01-24 01:36:28 +0900

    powerShell表記を追加

--- killLamp.bat
+++ killLamp.bat
@@ -1,12 +1,12 @@
-rem forestOfLamp終了用バッチファイル
+rem SYSTEM EXIT
 
-rem forestOfLamp_ledControlを終了
+rem forestOfLamp_ledControl exit
 taskkill /IM forestOfLamp_ledControl_debug.exe
 
-rem forestOfLamp_ledControlを終了
+rem forestOfLamp_ledControl exit
 taskkill /IM forestOfLamp_sound_debug.exe
 
-rem forestOfLamp_MySQLを終了
+rem forestOfLamp_MySQL exit
 taskkill /IM forestOfLamp_MySQL_debug.exe
 
 powershell sleep 3
--- startLamp.bat
+++ startLamp.bat
@@ -1,12 +1,12 @@
-rem forestOfLamp起動用バッチファイル
+rem SYSTEM START
 
-rem forestOfLamp_ledControlを起動
+rem forestOfLamp_ledControl start
 start "" "C:\Users\Masashi Seki\source\repos\forestOfLamp_ledControl\forestOfLamp_ledControl\bin\forestOfLamp_ledControl_debug.exe"
 
-rem forestOfLamp_ledControlを起動
+rem forestOfLamp_ledControl start
 start "" "C:\Users\Masashi Seki\source\repos\forestOfLamp_sound\forestOfLamp_sound\bin \forestOfLamp_sound_debug.exe"
 
-rem forestOfLamp_MySQLを起動
+rem forestOfLamp_MySQL start
 start "" "C:\Users\Masashi Seki\source\repos\forestOfLamp_MySQL\forestOfLamp_MySQL\bin\forestOfLamp_MySQL_debug.exe"
 
 powershell sleep 3
