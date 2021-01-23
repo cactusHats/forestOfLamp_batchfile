498cd64e8ce22fbc91f5d9fff2482e5dc9eccae6
cactusHats <mickeymasashi@gmail.com>
2021-01-24 01:35:52 +0900

    日本語表記を排除

--- killLamp.bat
+++ killLamp.bat
@@ -1,5 +1,4 @@
 rem forestOfLamp終了用バッチファイル
-@echo off
 
 rem forestOfLamp_ledControlを終了
 taskkill /IM forestOfLamp_ledControl_debug.exe
@@ -9,3 +8,5 @@ taskkill /IM forestOfLamp_sound_debug.exe
 
 rem forestOfLamp_MySQLを終了
 taskkill /IM forestOfLamp_MySQL_debug.exe
+
+powershell sleep 3
--- startLamp.bat
+++ startLamp.bat
@@ -1,5 +1,4 @@
 rem forestOfLamp起動用バッチファイル
-rem @echo off
 
 rem forestOfLamp_ledControlを起動
 start "" "C:\Users\Masashi Seki\source\repos\forestOfLamp_ledControl\forestOfLamp_ledControl\bin\forestOfLamp_ledControl_debug.exe"
@@ -10,4 +9,4 @@ start "" "C:\Users\Masashi Seki\source\repos\forestOfLamp_sound\forestOfLamp_sou
 rem forestOfLamp_MySQLを起動
 start "" "C:\Users\Masashi Seki\source\repos\forestOfLamp_MySQL\forestOfLamp_MySQL\bin\forestOfLamp_MySQL_debug.exe"
 
-sleep 3
+powershell sleep 3
